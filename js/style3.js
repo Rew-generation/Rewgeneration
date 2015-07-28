384fd084c399c5e2844e68e33c1db31616c51ae5
hyuito <0303hyuito@gmail.com>
2015-07-28 22:17:30 +0900

    サイドバー固定（暫定）
    サイドバー固定（暫定）

--- js/style3.js
+++ js/style3.js
@@ -1,10 +1,12 @@
 $(function(){
-    $(window).scroll(function(){
-        var now = $(window).scrollTop();
-        if(now > 1050 ){
-            $('#recommendbar').css({'position':'fixed', 'top':'0px', 'width':'27%', 'height':'100%', 'margin-top':'3%'});
-        }else{
-            $('#recommendbar').css({'position':'', 'top':'', 'width':'', 'margin-top':''});
-        }
+    var nav    = $('#recommendbar'),
+        offset = nav.offset();
+      
+    $(window).scroll(function () {
+      if($(window).scrollTop() > offset.top) {
+        nav.css({'position':'fixed', 'top':'0px', 'width':'27%', 'height':'100%', 'margin-top':'0%'});
+      } else {
+        nav.css({'position':'', 'top':'', 'width':'', 'margin-top':''});
+      }
     });
 });
