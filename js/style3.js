baced74d1c5626db2d53a2c5c5c9f3653ccc3d99
hyuito <0303hyuito@gmail.com>
2015-07-28 21:46:28 +0900

    サイドバー固定（暫定）
    サイドーバー固定（暫定）

--- js/style3.js
+++ js/style3.js
@@ -1,8 +1,10 @@
 $(function(){
-    $('#pickup').infiniteslide({
-        
-        'speed': 30, //速さ
-        'direction' : 'left', //向き
-        'pauseonhover': true //マウスオーバーでストップ
+    $(window).scroll(function(){
+        var now = $(window).scrollTop();
+        if(now > 1050 ){
+            $('#recommendbar').css({'position':'fixed', 'top':'0px', 'width':'27%', 'height':'100%', 'margin-top':'3%'});
+        }else{
+            $('#recommendbar').css({'position':'', 'top':'', 'width':'', 'margin-top':''});
+        }
     });
 });
