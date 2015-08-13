154e551434b22baf9d7f7b90a731a7c0d2a2b93f
hyuito <0303hyuito@gmail.com>
2015-08-14 00:46:33 +0900

    トップ、記事一覧、記事ページの完成
    トップ、記事一覧、記事ページの完成

---
+++ js/style4.js
@@ -0,0 +1,41 @@
+$(function() {
+ 
+ 
+// トップ画のアニメーションスピードをコントロール
+ 
+var fadeinSP = 500; //フェードインスピード
+var fadeoutSP = 300; //フェードアウトスピード
+ 
+ 
+// トップ画の通常非表示設定
+ 
+$('.recommend01').hide();
+$('.recommend02').hide();
+$('.recommend03').hide();
+$('.recommend04').hide();
+
+$('#aboutus a').hover(
+    function () {
+        $('.recommend02').stop(true, true).fadeIn(0);
+        }, function () {
+        $('.recommend02').stop(true, true).fadeOut(0);
+    }
+);
+
+$('#article a, .toparticlenext').hover(
+    function () {
+        $('.recommend03').stop(true, true).fadeIn(0);
+        }, function () {
+        $('.recommend03').stop(true, true).fadeOut(0);
+    }
+);
+
+$('#contact a').hover(
+    function () {
+        $('.recommend04').stop(true, true).fadeIn(0);
+        }, function () {
+        $('.recommend04').stop(true, true).fadeOut(0);
+    }
+);
+
+});
